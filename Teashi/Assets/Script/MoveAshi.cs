6c12dc83d0c9acd285a3fc0b3daa289e1ce3f6c1
sisizanohito <sisizanohito@gmail.com>
2019-08-12 01:58:28 +0900

    walktest char joint

---
+++ Teashi/Assets/Script/MoveAshi.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveAshi : MonoBehaviour
+{
+    public GameObject kata;
+    public GameObject te;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vec3 = te.transform.position - kata.transform.position;
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.down, vec3);
+    }
+}
