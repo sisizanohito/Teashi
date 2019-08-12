8bf1736a821ae46365c274908a9498215d3a7814
sisizanohito <sisizanohito@gmail.com>
2019-08-13 01:29:36 +0900

    物理は捨てた

--- Teashi/Assets/Script/MoveAshi.cs
+++ Teashi/Assets/Script/MoveAshi.cs
@@ -6,6 +6,9 @@ public class MoveAshi : MonoBehaviour
 {
     public GameObject kata;
     public GameObject te;
+    public GameObject jointPoint;
+
+    public float teLength = 1.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +18,13 @@ public class MoveAshi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        {//条件によってOFF
+            transform.rotation = jointPoint.transform.rotation;
+            transform.position = jointPoint.transform.position;
+        }
         Vector3 vec3 = te.transform.position - kata.transform.position;
         this.transform.rotation = Quaternion.FromToRotation(Vector3.down, vec3);
+
+        transform.localScale = new Vector3(1.0f, teLength + vec3.sqrMagnitude * 4.0f, 1.0f);
     }
 }
