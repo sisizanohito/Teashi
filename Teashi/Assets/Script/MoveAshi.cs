6d60466e5a07913b1eb8c5f6ce147afc21b88400
sisizanohito <sisizanohito@gmail.com>
2019-08-15 09:24:51 +0900

    とりあえず移動系はこれで

--- Teashi/Assets/Script/MoveAshi.cs
+++ Teashi/Assets/Script/MoveAshi.cs
@@ -8,7 +8,15 @@ public class MoveAshi : MonoBehaviour
     public GameObject te;
     public GameObject jointPoint;
 
+    public MoveChara moveChara;//手で移動するオブジェクト
+
     public float teLength = 1.0f;
+
+    [SerializeField]
+    private LayerMask layerMask = 0;
+
+    private bool hitting = false;
+    private Vector3 preHitPos = new Vector3();
     // Start is called before the first frame update
     void Start()
     {
@@ -18,13 +26,40 @@ public class MoveAshi : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        {//条件によってOFF
+        Vector3 kataTote = te.transform.position - kata.transform.position;
+        float teLen = teLength + kataTote.sqrMagnitude * 4.0f;
+        if (hitting)
+        {
+            teLen += 0.1f;
+        }
+        //床の判定
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hit;
+
+        Debug.DrawRay(ray.origin, ray.direction * teLen, Color.red);
+        if (Physics.Raycast(ray, out hit ,teLen, layerMask))
+        {
+            Vector3 hitPos = hit.point;
+            if (hitting)//初回でなければ
+            {
+                Vector3 moveVec =  preHitPos- hitPos;
+                //moveVec.y = 0.0f;
+                moveChara.Move(moveVec*50.0f);
+            }
+            //Debug.Log(transform.name + ":" + hitting);
+            preHitPos = hitPos;
+            hitting = true;
+        }
+        else{//条件によってOFF
+            hitting = false;
             transform.rotation = jointPoint.transform.rotation;
             transform.position = jointPoint.transform.position;
-        }
-        Vector3 vec3 = te.transform.position - kata.transform.position;
-        this.transform.rotation = Quaternion.FromToRotation(Vector3.down, vec3);
 
-        transform.localScale = new Vector3(1.0f, teLength + vec3.sqrMagnitude * 4.0f, 1.0f);
+        }
+        
+        this.transform.rotation = Quaternion.FromToRotation(Vector3.forward, kataTote);
+
+        //腕のサイズ変更
+        transform.localScale = new Vector3(1.0f, 1.0f, teLen);
     }
 }
---
+++ Teashi/Assets/Script/MoveChara.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveChara : MonoBehaviour
+{
+    public Transform rayPosition;
+
+    private Rigidbody m_rigidbody;
+    private bool isGround;
+    private bool isMoving;
+    public bool IsGround { get => isGround; }
+    public bool IsMoving { get => isMoving; set => isMoving = value; }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_rigidbody = GetComponent<Rigidbody>();
+        isGround = true;
+        isMoving = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Physics.Linecast(rayPosition.position, transform.position + (Vector3.down * 0.5f)))
+        {
+            isGround = true;
+        }
+        else
+        {
+            isGround = false;
+        }
+
+        //　接地確認用にレイを視覚化
+        Debug.DrawLine(rayPosition.position, transform.position + (Vector3.down * 0.5f), Color.red);
+    }
+
+    private void LateUpdate()
+    {
+        if (isGround&&!IsMoving)
+        {
+            m_rigidbody.velocity = Vector3.zero;
+        }
+        isMoving = false;
+    }
+
+    public void Move(Vector3 moveVec)
+    {
+        isMoving = true;
+        m_rigidbody.velocity = moveVec;
+    }
+}
